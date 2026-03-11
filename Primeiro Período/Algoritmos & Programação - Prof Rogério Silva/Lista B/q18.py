10c260fcf08b4f9d7f5ca3ddb399966317859155
symon0750 <symon0750@gmail.com>
2026-03-11 20:12:17 +0000

    Questão 18 melhorada

--- Primeiro Período/Algoritmos & Programação - Prof Rogério Silva/Lista B/q18.py
+++ Primeiro Período/Algoritmos & Programação - Prof Rogério Silva/Lista B/q18.py
@@ -5,13 +5,18 @@
 # 4 – Divisão). 
 # Calcule e escreva o resultado dessa operaçãosobre os dois valores lido
 
-def validaop(msg):
+def valida(msg, mi=None, mx=None):
 
     while True:
         try:
-            op = int(input(msg))
-            if 1 <= op <= 4:
-                return op 
+            val = int(input(msg))
+            if mi is not None and mx is not None:
+                if mi <= val <= mx:
+                    return val
+                else:
+                    print("Digite valores nos intervalos especificados.")
+            else:
+                return val
         except ValueError:
             print("Parâmetros errados, bananão.")
 
@@ -25,22 +30,30 @@ def validaN(msg):
 
 def calcula(op, v1, v2):
 
+    operacoes = {1: "+", 2: "-", 3: " x ", 4: " / "}
+    simbolo = operacoes[op]
+
     if op == 1:
-        return f"A soma dos números {v1:.2f} + {v2:.2f} é {(v1+v2):.2f}."
+        res = v1 + v2
     elif op == 2:
-        return f"A subtração dos números {v1:.2f} - {v2:.2f} é {(v1-v2):.2f}."
+        res = v1 - v2
     elif op == 3:
-        return f"A multiplicação dos números {v1:.2f} x {v2:.2f} é {(v1*v2):.2f}."
-    else:
-        return f"A divisão dos números {v1:.2f} + {v2:.2f} é {(v1/v2):.2f}." if v2 != 0 else "O valor 2 é 0."
+        res = v1 * v2    
+    elif op == 4:
+        if v2 != 0:
+            res = v1 / v2
+        else:
+            return "Erro, divisão por 0."
+    
+    return f"Operação: {v1} {simbolo} {v2} = {res:.2f}"
 
 
 def main():
     
     while True:
-        op = validaop("Digite uma opção de 1~4: \n1 - Adição \n2 - Subtração \n3 - Multiplicação \n4 - Divisão\n")
-        v1 = validaN("Digite o 1° número: ")
-        v2 = validaN("Digite o 2° número: ")
+        op = valida("Digite uma opção de 1~4: \n1 - Adição \n2 - Subtração \n3 - Multiplicação \n4 - Divisão\n", 1, 4)
+        v1 = valida("Digite o 1° número: ")
+        v2 = valida("Digite o 2° número: ")
 
         res = calcula(op, v1, v2)
         print(res)
